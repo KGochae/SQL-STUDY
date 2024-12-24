488027d8a92d757c6834fc31a25acacd2ae91f54
liveandletlive <86241587+KGochae@users.noreply.github.com>
2024-12-24 12:29:06 +0900

    Update join.sql

--- SolveSQL/join.sql
+++ SolveSQL/join.sql
@@ -1,17 +1,9 @@
 # 3028 유저의 3명의 친구관계를 뽑아내는 쿼리
-# 가령, 아래의 경우  3020, 2, 3 세명의 id 가 친구관계임 
-user_a_id user_b_id
-    3020        2
-    3020        3
-    2           3
-
-
 
   
-SELECT 
-    t1.user_a_id AS user_a_id,
-    t1.user_b_id AS user_b_id,
-    t2.user_b_id AS user_c_id
+SELECT t1.user_a_id AS user_a_id
+    , t1.user_b_id AS user_b_id
+    , t2.user_b_id AS user_c_id
 FROM 
     edges t1
 JOIN 
@@ -23,5 +15,4 @@ JOIN
 WHERE 
     t3.user_a_id = t1.user_a_id
     AND 3820 IN (t1.user_a_id, t1.user_b_id, t2.user_b_id)
-    AND t1.user_a_id < t1.user_b_id
-    AND t1.user_b_id < t2.user_b_id
+    AND t1.user_a_id < t1.user_b_id < t2.user_b_id
